f80d31b193559bfdc339c0b3eed556a5462d3a0d
Francis Seneve <francisseneve@gmail.com>
2022-09-06 12:12:38 +0100

    Oauth setup

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,7 +5,7 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'python-js-golang-dev.us.auth0.com'
+AUTH0_DOMAIN = 'dev-francis-python.us.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'http://127.0.0.1:5000/'
 
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'python-js-golang-dev.us', // the auth0 domain prefix
+    url: 'dev-francis-python.us', // the auth0 domain prefix
     audience: 'http://127.0.0.1:5000/', // the audience set for the auth0 app
-    clientId: 'yqa0efTK5JESPsiYD3pf1e3mNwuv2Wjr', // the client id generated for the auth0 app
+    clientId: '5P2xkQDMuJitdxvs8TlrxOVWOBRsUrTr', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:4200', // the base url of the running ionic application.
   }
 };
